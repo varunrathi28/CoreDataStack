2d3ff7a80a6f0d9f23c81ae1146545032578d9e1
Varun Rathi <varun.rathi@jeevansathi.com>
2019-05-08 03:11:54 +0530

    Core Data manager added

---
+++ CoreDataManager.swift
@@ -0,0 +1,20 @@
+//
+//  CoreDataManager.swift
+//  CoreDataStack
+//
+//  Created by Varun Rathi on 08/05/19.
+//  Copyright © 2019 Varun Rathi. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+public class CoreDataManager {
+
+
+    public let modelName:String?
+    init(modelName:String) {
+    self.modelName = modelName
+}
+
+}
--- CoreDataStack/AppDelegate.swift
+++ CoreDataStack/AppDelegate.swift
@@ -13,9 +13,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    private let coreDataModel = CoreDataManager(modelName: "Model")
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        print(coreDataModel.managedObjectContext)
         return true
     }
 
