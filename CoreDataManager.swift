2169393aae0bb0e0cbcfbd3c7e48965e0c4cbfdc
Varun Rathi <varun.rathi@jeevansathi.com>
2019-05-08 03:12:51 +0530

    Managed object model added

--- CoreDataManager.swift
+++ CoreDataManager.swift
@@ -11,6 +11,17 @@ import CoreData
 
 public class CoreDataManager {
 
+    private lazy var managedObjectModel :NSManagedObjectModel = {
+        guard let modelURL = Bundle.main.url(forResource:self.modelName, withExtension:"momd") else {
+            fatalError("Unable to find Data Model")
+        }
+        guard let managedModel = NSManagedObjectModel(contentsOf: modelURL) else {
+            fatalError("Unable to load model")
+        }
+        return managedModel
+    }()
+    
+    
 
     public let modelName:String?
     init(modelName:String) {
